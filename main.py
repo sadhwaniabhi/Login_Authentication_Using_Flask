68ec1e1b0b562acc0af1c2d1a9c02aef571d54e8
Abhishek Sadhwani <abhisheksadhwani23@gmail.com>
2022-08-02 10:36:56 +0530

    User register function and download file function

--- main.py
+++ main.py
@@ -27,8 +27,18 @@ def home():
     return render_template("index.html")
 
 
-@app.route('/register')
+@app.route('/register', methods=["GET", "POST"])
 def register():
+    """function to register user and save their data to database using the request method of flask"""
+    if request.method == "POST":
+        new_user = User(email=request.form.get("email"),
+                        password=request.form.get("password"),
+                        name=request.form.get("name")
+                        )
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect(url_for("secrets", name=new_user.name))
     return render_template("register.html")
 
 
@@ -37,9 +47,9 @@ def login():
     return render_template("login.html")
 
 
-@app.route('/secrets')
-def secrets():
-    return render_template("secrets.html")
+@app.route('/secrets/<name>')
+def secrets(name):
+    return render_template("secrets.html", name=name)
 
 
 @app.route('/logout')
@@ -49,7 +59,10 @@ def logout():
 
 @app.route('/download')
 def download():
-    pass
+    """function to let user download the cheat sheet file"""
+    return send_from_directory(directory='static', path='files/cheat_sheet.pdf')
+
+# filename parameter has been renamed to path
 
 
 if __name__ == "__main__":
